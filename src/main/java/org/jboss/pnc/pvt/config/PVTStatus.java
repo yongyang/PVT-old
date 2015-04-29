b35ebcb8434033256875db077910d3137ac2b931
yyang <yyang@redhat.com>
2015-04-29 14:11:50 +0800

    add PVTStatus, User

---
+++ src/main/java/org/jboss/pnc/pvt/config/PVTStatus.java
@@ -0,0 +1,12 @@
+package org.jboss.pnc.pvt.config;
+
+/**
+ * Created by yyang on 4/24/15.
+ */
+public class PVTStatus {
+
+    public static enum Status {
+        ANALYZING, VERIFYING, PASSED, REJECTED
+    }
+
+}
---
+++ src/main/java/org/jboss/pnc/pvt/config/User.java
@@ -0,0 +1,8 @@
+package org.jboss.pnc.pvt.config;
+
+/**
+ * Created by yyang on 4/24/15.
+ */
+public class User {
+
+}
