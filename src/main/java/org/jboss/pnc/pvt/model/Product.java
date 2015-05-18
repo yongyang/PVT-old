f9c3e49bde3c1242ff9f0ca331fc26af9ee37b87
Lin Gao <lgao@redhat.com>
2015-05-18 14:34:00 +0800

    Added POJO and faked data for demo

---
+++ src/main/java/org/jboss/pnc/pvt/model/Product.java
@@ -0,0 +1,144 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright @year, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.pnc.pvt.model;
+
+import java.io.Serializable;
+
+/**
+ * <code>Product</code> represents a product stream, like: EAP 6.4.x, BRMS 6.0.x, etc.
+ * 
+ * @author <a href="mailto:lgao@redhat.com">Lin Gao</a>
+ *
+ */
+public class Product implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5742290247172000842L;
+
+    private long id;
+    
+    private String name;
+    
+    private String branch;
+    
+    private String mavenRepo;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the branch
+     */
+    public String getBranch() {
+        return branch;
+    }
+
+    /**
+     * @param branch the branch to set
+     */
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    /**
+     * @return the mavenRepo
+     */
+    public String getMavenRepo() {
+        return mavenRepo;
+    }
+
+    /**
+     * @param mavenRepo the mavenRepo to set
+     */
+    public void setMavenRepo(String mavenRepo) {
+        this.mavenRepo = mavenRepo;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((branch == null) ? 0 : branch.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (branch == null) {
+            if (other.branch != null)
+                return false;
+        } else if (!branch.equals(other.branch))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", branch=" + branch + ", mavenRepo=" + mavenRepo + "]";
+    }
+    
+}
