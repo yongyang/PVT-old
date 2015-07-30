5372bb57728ec3ecab220d2d6f90c4caee6ca344
yyang <yyang@redhat.com>
2015-07-30 14:59:20 +0800

    Product list OK

--- src/main/java/org/jboss/pnc/pvt/model/Product.java
+++ src/main/java/org/jboss/pnc/pvt/model/Product.java
@@ -141,11 +141,7 @@ public class Product implements Serializable {
              */
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+        return id.hashCode();
     }
 
     /* (non-Javadoc)
