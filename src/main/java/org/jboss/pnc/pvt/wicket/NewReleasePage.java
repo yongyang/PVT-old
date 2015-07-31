2c97307c61c09a9466ac29d448231a29ae21c503
yyang <yyang@redhat.com>
2015-07-31 11:49:24 +0800

    New Release page

---
+++ src/main/java/org/jboss/pnc/pvt/wicket/NewReleasePage.java
@@ -0,0 +1,71 @@
+package org.jboss.pnc.pvt.wicket;
+
+import com.googlecode.wicket.kendo.ui.form.TextArea;
+import com.googlecode.wicket.kendo.ui.form.TextField;
+import org.apache.wicket.Application;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.ValidationError;
+import org.jboss.pnc.pvt.dao.PVTDataAccessObject;
+import org.jboss.pnc.pvt.model.Product;
+
+/**
+ * @author <a href="mailto:yyang@redhat.com">Yong Yang</a>
+ */
+public class NewReleasePage extends TemplatePage {
+
+    private Product newProduct = new Product();
+
+    public NewReleasePage() {
+        setActiveMenu("releases");
+
+        add(new FeedbackPanel("feedbackMessage"));
+
+        Form newProductForm = new Form("form-newproduct", new CompoundPropertyModel(newProduct)) {
+            @Override
+            protected void onSubmit() {
+                System.out.println("Submit: " + newProduct);
+                PVTDataAccessObject dao = ((PVTApplication) Application.get()).getDAO();
+                dao.getPvtModel().addProduct(newProduct);
+                dao.persist();
+
+                setResponsePage(new ProductsPage("Product: " + newProduct.getName() + " Created."));
+            }
+        };
+
+        TextField<String> nameTextField = new TextField<String>("name");
+        nameTextField.setRequired(true);
+        nameTextField.add(new IValidator<String>() {
+            @Override
+            public void validate(IValidatable<String> validatable) {
+                String inputName = validatable.getValue();
+                PVTDataAccessObject dao = ((PVTApplication) Application.get()).getDAO();
+                boolean existed = false;
+                for(Product p : dao.getPvtModel().getProducts()){
+                    if(p.getName().equalsIgnoreCase(inputName)) {
+                        existed = true;
+                        break;
+                    }
+                }
+                if(existed) {
+                    ValidationError error = new ValidationError(this);
+                    error.setMessage("Product " + inputName + " is already existed");
+                    validatable.error(error);
+                }
+            }
+        });
+        newProductForm.add(nameTextField);
+        newProductForm.add(new TextField<String>("packages"));
+        newProductForm.add(new TextField<String>("maintainer"));
+        newProductForm.add(new TextField<String>("developer"));
+        newProductForm.add(new TextField<String>("qe"));
+        newProductForm.add(new TextArea<String>("description"));
+
+        add(newProductForm);
+
+
+    }
+}
--- src/main/java/org/jboss/pnc/pvt/wicket/ReleasePage.java
+++ src/main/java/org/jboss/pnc/pvt/wicket/ReleasePage.java
@@ -1,11 +1,29 @@
 package org.jboss.pnc.pvt.wicket;
 
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 /**
  * Created by yyang on 5/5/15.
  */
 public class ReleasePage extends TemplatePage {
 
     public ReleasePage() {
-        super();
+        this("PVT release loaded!");
+    }
+
+    public ReleasePage(String info) {
+        super(info);
+
+        add(new Link<String>("link-newrelease") {
+            @Override
+            public void onClick() {
+                PageParameters pp = new PageParameters();
+                pp.set(0, "a");
+                pp.set(1,"b");
+                pp.add("c","d");
+                setResponsePage(NewReleasePage.class, pp);
+            }
+        });
     }
 }
