011aa5cac9c470cb08c265cb924e25ee8cc6473a
Lin Gao <lgao@redhat.com>
2015-09-18 14:18:33 +0800

    Validate jdk selection for JDKcompatible tool

--- src/main/java/org/jboss/pnc/pvt/wicket/JDKCompatibleVerifyToolNewPage.java
+++ src/main/java/org/jboss/pnc/pvt/wicket/JDKCompatibleVerifyToolNewPage.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.validation.IValidatable;
@@ -14,14 +16,13 @@ import org.jboss.pnc.pvt.model.JDKCompatibleVerifyTool;
 import org.jboss.pnc.pvt.model.JDKCompatibleVerifyTool.JDK;
 import org.jboss.pnc.pvt.model.VerifyTool;
 
-
 /**
  * A web-page used to edit/create a Verification Tool.
  * 
  * @author <a href="mailto:lgao@redhat.com">Lin Gao</a>
  *
  */
-@SuppressWarnings({ "serial"})
+@SuppressWarnings({ "serial" })
 public class JDKCompatibleVerifyToolNewPage extends AbstractVerifyToolPage {
 
     private static final long serialVersionUID = 1L;
@@ -33,8 +34,22 @@ public class JDKCompatibleVerifyToolNewPage extends AbstractVerifyToolPage {
     public JDKCompatibleVerifyToolNewPage(PageParameters pp, String info) {
         super(pp, info);
 
-        JDKCompatibleVerifyTool jdkVerifyTool = (JDKCompatibleVerifyTool)tool;
-        DropDownChoice<JDK> minJDKDropDownChoice = new DropDownChoice<JDK>("minJDK", Arrays.asList(JDK.values())){
+        JDKCompatibleVerifyTool jdkVerifyTool = (JDKCompatibleVerifyTool) tool;
+
+        final IChoiceRenderer<JDK> JDKDropChoiseRender = new IChoiceRenderer<JDK>() {
+            @Override
+            public Object getDisplayValue(JDK jdk) {
+                return jdk.name();
+            }
+
+            @Override
+            public String getIdValue(JDK jdk, int index) {
+                return jdk.name();
+            }
+
+        };
+        DropDownChoice<JDK> maxJDKDropDownChoice = new DropDownChoice<JDK>("maxJDK", Model.of(jdkVerifyTool.getMaxJDK()),
+                Arrays.asList(JDK.values()), JDKDropChoiseRender) {
 
             @Override
             public boolean isNullValid() {
@@ -42,15 +57,23 @@ public class JDKCompatibleVerifyToolNewPage extends AbstractVerifyToolPage {
             }
 
             @Override
-            protected boolean wantOnSelectionChangedNotifications() {
-                return true;
+            protected void onModelChanged() {
+                jdkVerifyTool.setMaxJDK(getModelObject());
+            }
+        };
+        maxJDKDropDownChoice.setRequired(true);
+
+        DropDownChoice<JDK> minJDKDropDownChoice = new DropDownChoice<JDK>("minJDK", Model.of(jdkVerifyTool.getMinJDK()),
+                Arrays.asList(JDK.values()), JDKDropChoiseRender) {
+
+            @Override
+            public boolean isNullValid() {
+                return false;
             }
 
             @Override
-            protected void onSelectionChanged(JDK newSelection) {
-                super.onSelectionChanged(newSelection);
-                jdkVerifyTool.setMinJDK(newSelection);
-                setModelObject(newSelection);
+            protected void onModelChanged() {
+                jdkVerifyTool.setMinJDK(getModelObject());
             }
         };
         minJDKDropDownChoice.setRequired(true);
@@ -59,41 +82,19 @@ public class JDKCompatibleVerifyToolNewPage extends AbstractVerifyToolPage {
             @Override
             public void validate(IValidatable<JDK> validatable) {
                 JDK jdk = validatable.getValue();
-                if (jdk.compareTo(jdkVerifyTool.getMaxJDK()) > 0) {
+                if (jdk.compareTo(JDK.valueOf(maxJDKDropDownChoice.getValue())) > 0) {
                     ValidationError error = new ValidationError();
-                    error.setMessage("Min JDK Version MUST be smaller than Max JDK Version");
+                    error.addKey("minjdk.maxjdk").setVariable("minJDK", jdk.name())
+                            .setVariable("maxJDK", maxJDKDropDownChoice.getValue());
                     validatable.error(error);
                 }
             }
         });
 
-        DropDownChoice<JDK> maxJDKDropDownChoice = new DropDownChoice<JDK>("maxJDK", Arrays.asList(JDK.values())){
-
-            @Override
-            public boolean isNullValid() {
-                return false;
-            }
-
-            @Override
-            protected boolean wantOnSelectionChangedNotifications() {
-                return true;
-            }
-
-            @Override
-            protected void onSelectionChanged(JDK newSelection) {
-                super.onSelectionChanged(newSelection);
-                jdkVerifyTool.setMaxJDK(newSelection);
-                setModelObject(newSelection);
-            }
-        };
-        maxJDKDropDownChoice.setRequired(true);
-
         form.add(minJDKDropDownChoice);
         form.add(maxJDKDropDownChoice);
         form.add(new CheckBox("fastReturn", new PropertyModel<Boolean>(jdkVerifyTool, "fastReturn")));
-        
-        minJDKDropDownChoice.setModelObject(jdkVerifyTool.getMinJDK());
-        maxJDKDropDownChoice.setModelObject(jdkVerifyTool.getMaxJDK());
+
     }
 
     @Override
